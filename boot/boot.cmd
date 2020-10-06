b23198fc99e2e32c5b586f9dfea0d751f0a55e70
Ewout Prangsma <ewout@prangsma.net>
2020-10-06 10:03:51 +0200

    loglevel 7

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -1,5 +1,5 @@
 tftp ${kernel_addr_r} zImage
 tftp ${fdt_addr_r} sunxi-orangepizero.dtb
 tftp ${ramdisk_addr_r} uInitrd
-setenv bootargs loglevel=6 console=tty1 console=ttyS0,115200 ip=dhcp root=/dev/root rootwait panic=2
+setenv bootargs loglevel=7 console=tty1 console=ttyS0,115200 ip=dhcp root=/dev/root rootwait panic=2
 bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
