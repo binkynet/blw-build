2d23cbb45079bd362e172985ca8f140ece38c618
Ewout Prangsma <ewout@prangsma.net>
2026-03-29 21:02:47 +0200

    fixcmdline

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -2,5 +2,5 @@ setenv netretry yes
 tftp ${kernel_addr_r} zImage
 tftp ${fdt_addr_r} sunxi-orangepizero.dtb
 tftp ${ramdisk_addr_r} uInitrd
-setenv bootargs loglevel=6 console=tty1 console=ttyS0,115200 root=/dev/root rootwait panic=2 ip=192.168.77.10::192.168.77.1:255.255.255.0:dcc:eth0:off"
+setenv bootargs loglevel=6 console=tty1 console=ttyS0,115200 root=/dev/root rootwait panic=2 ip=192.168.77.10::192.168.77.1:255.255.255.0:dcc:eth0:off
 bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
