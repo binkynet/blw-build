f6a87c6f08456891877ef4115c94eb13baeb15f3
Ewout Prangsma <ewout@prangsma.net>
2026-03-29 20:46:57 +0200

    nodhcp

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -2,5 +2,5 @@ setenv netretry yes
 tftp ${kernel_addr_r} zImage
 tftp ${fdt_addr_r} sunxi-orangepizero.dtb
 tftp ${ramdisk_addr_r} uInitrd
-setenv bootargs loglevel=6 console=tty1 console=ttyS0,115200 root=/dev/root rootwait panic=2 ip=dhcp
+setenv bootargs loglevel=6 console=tty1 console=ttyS0,115200 root=/dev/root rootwait panic=2
 bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
